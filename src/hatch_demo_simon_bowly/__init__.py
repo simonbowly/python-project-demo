44ec1387d3adeedb14a483cc896468e14a4e27ae
Simon Bowly <bowly@gurobi.com>
2022-10-25 15:51:56 +1100

    Rework workflows again, move to src subdir

--- src/hatch_demo_simon_bowly/__init__.py
+++ src/hatch_demo_simon_bowly/__init__.py
@@ -1,3 +1,6 @@
 # SPDX-FileCopyrightText: 2022-present Simon Bowly <bowly@gurobi.com>
 #
 # SPDX-License-Identifier: MIT
+
+def do_the_thing():
+    print("I did the thing you asked!")
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,8 @@
 import unittest
 
+import hatch_demo_simon_bowly
+
 class Test(unittest.TestCase):
 
     def test_simple(self):
-        pass
+        hatch_demo_simon_bowly.do_the_thing()
